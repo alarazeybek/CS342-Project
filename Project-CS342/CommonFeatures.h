c8f32e771fee1786565315e755af4d5a2bc3eee4
unknown <alarazeybek@gmail.com>
2023-10-20 16:32:30 +0300

    t

--- Project-CS342/CommonFeatures.h
+++ Project-CS342/CommonFeatures.h
@@ -10,7 +10,7 @@
 // Checking if the number is prime or not.
 bool IsPrimeNumber(const int num);
 // Parsing the command line arguments and reading the flag values for the rest of the program.
-void commandLineParsing(int argc, char *argv[], char *flag_n[100],  char *flag_m[100], char *flag_i, char *flag_o);
+void commandLineParsing(int argc, char *argv[], char *flag_n[100],  char *flag_m[100], char *flag_i[100], char *flag_o[100]);
 // Parsing the inout file into  N inter files.
 void openIntermediateFiles(char *inputFileName, char* inter_files[], const int child_process_num);
 // Deleting the inter files.
