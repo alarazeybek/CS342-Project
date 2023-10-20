4220431e67a09f58f4c2de01543f7641294929c9
unknown <alarazeybek@gmail.com>
2023-10-20 20:06:00 +0300

    t

--- Project-CS342/primeP.c
+++ Project-CS342/primeP.c
@@ -148,7 +148,7 @@ void ProcessHandling(const int p_child_num, const int message_size, char* inter_
                          c = fgetc(file);
                      }*/
                 // Convert the line to an integer
-                //int number = atoi(line);
+                int number = atoi(line);
                 if (IsPrimeNumber(number)){
                     // Create message
                     itemp = (struct item *) bufferp;
