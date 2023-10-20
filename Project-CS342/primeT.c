1973d36a506fd3c4df2d87e94cdf87fcdaef84ec
unknown <alarazeybek@gmail.com>
2023-10-20 14:31:38 +0300

    Part B almost do

--- Project-CS342/primeT.c
+++ Project-CS342/primeT.c
@@ -49,7 +49,7 @@ void DeleteNode(struct LinkedList* list) {
 }
 // Define a struct to pass data to the worker threads.
 struct ThreadData {
-    char* inputFileName;
+    char* interFileName;
     int threadNumber;
     //struct LinkedList* link;
     //pthread_mutex_t* mutex;
@@ -57,20 +57,20 @@ struct ThreadData {
 // LinkedList
 
 LinkedList* PrimeList;
-
+static void *do_task(void *arg_ptr);
 int main(int argc, char* argv[]) {
     PrimeList = createLinkedList();
     // Command Line Parsing:
-    int *child_thread_num = (int*)malloc(sizeof(int);
-    int *prime_num_in_message = (int*)malloc(sizeof(int);
+    int *child_thread_num = (int*)malloc(sizeof(int));
+    int *prime_num_in_message = (int*)malloc(sizeof(int));
     char input_file_name[100];
     char output_file_name[100];
     commandLineParsing(argc,argv,*child_thread_num,*prime_num_in_message,input_file_name,
                        output_file_name);
 
     // Split the input file into N intermediate input files.
-    char* inter_files[*child_process_num];
-    openIntermediateFiles(input_file_name, inter_files, *child_thread_num_num);
+    char* inter_files[*child_thread_num];
+    openIntermediateFiles(input_file_name, inter_files, *child_thread_num);
     // Create an array of thread handles and thread data.
     pthread_t tids[*child_thread_num];
     struct ThreadData t_args[*child_thread_num];
@@ -82,7 +82,7 @@ int main(int argc, char* argv[]) {
 */
     // Create and start worker threads.
     for (int i = 0; i < *child_thread_num; i++) {
-        t_args[i].inputFileName = inter_files[i];/* assign intermediate file name */;
+        t_args[i].interFileName = inter_files[i];/* assign intermediate file name */;
         t_args[i].threadNumber = i;
         //t_args[i].primeList = /* create a thread-specific linked list */;
         //t_args[i].mutex = &mutex;
@@ -122,25 +122,25 @@ int main(int argc, char* argv[]) {
 // this is the function to be executed by all the threads concurrently
 static void *do_task(void *arg_ptr)
 {
-    int i;
-    FILE *fp;
-    char filename[MAXFILENAME];
     char *retreason;
-
-    printf("thread %d started\n", ((struct arg *) arg_ptr)->t_index);
 
     sprintf(filename, "output_of_thread%d.txt",
             ((struct arg *) arg_ptr)->t_index);
 
-    fp = fopen(filename, "w");
+    FILE *fp = fopen(((struct ThreadData *) arg_ptr)->interFileName, "w");
     if (fp == NULL) {
         perror("do_task:");
         exit(1);
     }
 
-    for (i = ((struct arg *) arg_ptr)->n;
-         i <= ((struct arg *) arg_ptr)->m; ++i) {
-        fprintf(fp, "integer = %d\n", i);
+    char line[100];
+    while (fgets(line, sizeof(line), fp)) {
+        // pthread_mutex_lock(threadData->mutex);
+        int number = atoi(line);
+        if (IsPrimeNumber(number)){
+            insert(PrimeList, number);
+        }
+        //pthread_mutex_unlock(threadData->mutex);
     }
 
     fclose(fp);
@@ -151,31 +151,4 @@ static void *do_task(void *arg_ptr)
     // we could simple exit as below, if we don't want to pass a reason
     // pthread_exit(NULL);
     // then we would also modify pthread_join call.
-}
-
-// Thread function to process an intermediate file.
-void* ProcessIntermediateFile(void* data) {
-    struct ThreadData* threadData = (struct ThreadData*)data;
-
-    FILE* inputFile = fopen(threadData->inputFileName, "r");
-    if (!inputFile) {
-        perror("Error opening input file");
-        pthread_exit(NULL);
-    }
-
-    char line[100];
-    while (fgets(line, sizeof(line), inputFile)) {
-        int number = atoi(line);
-        if (IsPrime(number)) {
-            pthread_mutex_lock(threadData->mutex);
-            // Update the prime list with the prime number.
-            // You can also implement frequency counting here.
-            // Make sure to use the mutex to protect the list.
-            // ...
-            pthread_mutex_unlock(threadData->mutex);
-        }
-    }
-
-    fclose(inputFile);
-    pthread_exit(NULL);
 }
