aa609e72c112c1443a894498239aef14bc3cb80b
unknown <alarazeybek@gmail.com>
2023-10-20 16:08:03 +0300

    t

--- Project-CS342/primeT.c
+++ Project-CS342/primeT.c
@@ -57,7 +57,7 @@ struct ThreadData {
 };
 // LinkedList
 
-Stuct LinkedList* PrimeList;
+stuct LinkedList* PrimeList;
 static void *do_task(void *arg_ptr);
 int main(int argc, char* argv[]) {
     PrimeList = createLinkedList();
